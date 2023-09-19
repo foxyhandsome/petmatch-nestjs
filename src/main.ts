c6c2687256380d31c38ebe624af87e8a5f1b21db
kaoukung <kaou913@gmail.com>
2023-09-19 16:26:26 +0700

    add

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:4200'); // Replace with your Angular app's URL
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Credentials', 'true'); // Set to true if you're using cookies or sessions
+    next();
+  });
   await app.listen(3000);
 }
 bootstrap();
+
